5ea8bfcbc175e05996b8e01b78571c727b0f0857
PDrookie <jim510199@gmail.com>
2022-06-06 01:16:28 +0800

    seventh upload

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -153,6 +153,9 @@ public class Playing extends State implements Statemethods {
 		case KeyEvent.VK_SPACE:
 			player.setJump(true);
 			break;
+		case KeyEvent.VK_K:
+			player.setAttacking(true);
+			break;
 		case KeyEvent.VK_ESCAPE:
 			paused = !paused;
 			break;
